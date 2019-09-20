766d639112dca7bd8047883e58ee345afa334b22
Alain <youpsla@gmail.com>
2019-09-20 11:11:59 +0200

    reweets ok

--- retweets.py
+++ retweets.py
@@ -5,9 +5,11 @@ from aiopg.sa import create_engine
 from atomdb.sql import SQLModelManager
 from models import User, Tweet
 import operator
+from datetime import datetime
 
 
 USER_SCREEN_NAME = "deviantcoin"
+TWEET_ID = '1174595009674448898'
 ### Twitter access tokens ###
 consumer_key = os.environ["TWITTER_CONSUMER_KEY"]
 consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
@@ -20,7 +22,6 @@ api = tweepy.API(auth_handler=auth, retry_count=3)
 
 
 async def get_last_retweet_from_db():
-    print('dada')
     async with create_engine(
         user="youpsla",
         database="deviant",
@@ -33,13 +34,24 @@ async def get_last_retweet_from_db():
     
         last_retweet = await Tweet.objects.all()
     ordered = sorted(last_retweet, key=operator.attrgetter('created_at'), reverse = True)
-    for i in ordered:
-        print(i.created_at)
-    print(ordered[0])
+    if not ordered:
+        print('Database is empty')
+        return None
+    print(f'Last tweet in DB {ordered[0].created_at}')
     return ordered[0]
 
 
 async def update_db():
+    last_retweet_from_db = await get_last_retweet_from_db()
+    retweets_from_twitter = api.retweets(TWEET_ID, count = 100)
+    #print(type(last_retweet_from_db.created_at))
+    if not last_retweet_from_db:
+        new_retweets = [r for r in retweets_from_twitter if r.created_at > datetime(2019, 8, 31,0,0)]
+    else:
+        new_retweets = [r for r in retweets_from_twitter if r.created_at > last_retweet_from_db.created_at]
+    print(f'{len(new_retweets)} retweets to insert in db')
+
+
     async with create_engine(
         user="youpsla",
         database="deviant",
@@ -49,28 +61,33 @@ async def update_db():
     ) as engine:
         mgr = SQLModelManager.instance()
         mgr.database = engine
-        mgr.create_tables()
-
-
-        last_retweet_from_db = await get_last_retweet_from_db()
-        retweets_from_twitter = api.retweets('1174595009674448898', count = 10)
-        new_retweets = [r for r in retweets_from_twitter if r.created_at > last_retweet_from_db.created_at]
-        for i in new_retweets:
-            print(i.created_at)
-        #print(new_retweets)
 
         for retweet in new_retweets:
+            user, created = await User.objects.get_or_create(id=int(retweet.user.id))
+            if created:
+                user.name = retweet.user.name
+                user.screen_name = retweet.user.screen_name
+                user.followers_count = retweet.user.followers_count
+                user.statuses_count = retweet.user.statuses_count
+                await user.save()
+            
+
             new_record = Tweet()
             new_record.id = retweet.id
             new_record.text = retweet.text
             new_record.created_at = retweet.created_at
-            new_record.user = retweet.user
+            new_record.user = user
+            new_record.retweet = True
+            await new_record.save(force_insert=True)
 
 
 
-
+async def main():
+    while True:
+        await asyncio.sleep(20)
+        await update_db()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    task = loop.create_task(update_db())
+    task = loop.create_task(main())
     loop.run_until_complete(task)
